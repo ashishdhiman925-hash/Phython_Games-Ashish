37872b49b9d933b46e321309e749c6d829a3449e
ashishdhiman925-hash <ashishdhiman925@gmail.com>
2025-10-09 07:37:50 +0530

    Update python.py

--- python.py
+++ python.py
@@ -84,6 +84,34 @@ elif user_input == 2:
 # Computer choose
 print(f"computer choice: {list[computer_choice]} \n ")
 # Comparision to check if computer wins or users
+
+# Password generator
+
+
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+# Easy Way out
+password = ""
+#nr_letter = 4
+for char in range(1, nr_letters + 1):
+    random_char = random.choice(letters)
+    password = password + random_char
+
+for char in range(1,nr_symbols + 1):
+    random_sym = random.choice(symbols)
+    password = password + random_sym
+
+for char in range(1,nr_numbers + 1):
+    random_num = random.choice(numbers)
+    password = password + random_num
+print(password)
 if computer_choice > user_input:
     print("Computer Wins \n ")
 elif user_input > computer_choice:
